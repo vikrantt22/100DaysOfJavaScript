d8120a131b38a12be7bb3b0f05efe053dba15f2f
vikrantt22 <106909296+vikrantt22@users.noreply.github.com>
2024-06-17 19:43:34 +0530

    learnt about the basic operations.

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -4,28 +4,63 @@ let age = 40
 // console.log(typeof(age));
 
 age = String(age);
-console.log(age);
-console.log(typeof(age));
+// console.log(age);
+// console.log(typeof(age));
 
 let isLoggedIn = true // it can be converted to 1 on number conversion
 // on converting empty string into boolean it gets false
 
 let numIsLoggedIn = Number(isLoggedIn)
-console.log(numIsLoggedIn);
+// console.log(numIsLoggedIn);
 
 // NaN => not a number is assigned when a string that cannot be converted to number is converted to number
 let name = "123vikrant"
 
 let numName = Number(name)
-console.log(numName);
+// console.log(numName);
 
 // undefined => undefined is also gets converted into NaN
 let sid = undefined
 
 let numsid = Number(sid)
-console.log(numsid);
+// console.log(numsid);
 
 // when null is converted into number is converts into 0
 let sname = null;
 let numSname = Number(sname);
-console.log(numSname);
+// console.log(numSname);
+
+// *************** Operations ***************
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3**3)
+// console.log(3/3)
+// console.log(3%3)
+
+let str1 = 'Vikrant'
+let str2 = ' Salunkhe'
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+
+//operator conversion can also be done by using + symbol
+// console.log(+true)
+// console.log(+false)
+
+//incremental operator
+
+let num1 = 10
+num1++
+console.log(num1); // 11 (first use value then increment)
+
+let num2 = 10
+++num2
+console.log(num2); // 11 (first increment then use value)
